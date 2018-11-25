5080fcbd79ae921de4f99bcf204e9bbe4c98eae7
Joram <10008525+derjoram@users.noreply.github.com>
2018-11-25 21:31:16 +0100

    Added export functionality
    Added json export to the fields with input. created sample json file.

--- GUI/GUI1.py
+++ GUI/GUI1.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python
 
+
+import json
+from pprint import pprint
+
+with open('data.json') as f:
+    data = json.load(f)
+
+exportdata = data
+
 # trying to import a module, fails on run, will figure out later
 #import mutagen
 
 # input area, add input values here that will later end up in the json file. for now it will just be command line, later it will get a UI
 
 title = input("Episode Title: ")
+exportdata["title"] = title
 
 subtitle = "<p>"
 subtitle += input("Subtitle: ")
 subtitle += "</p>"
+exportdata["subtitle"] = subtitle
+
 
 content = input("Episode description in HTML code: ")
+exportdata["content"] = content
 
 #trying to get length from mp3 but mutagen can't be loaded
 """
@@ -21,37 +34,19 @@ print(audio.info.length)
 """
 
 episode = input("Episode number (three digits): ")
+exportdata["episode"] = episode
 
 filename = input("Filename (Slug): ")
+exportdata["filename"] = filename
+
 
 humandate = input("Publication date (DD.MM.YYYY): ")
-"""{
-		"chapters": [
-		    {
-		        "image": null,
-		        "start": "00:22:46.190",
-		        "title": "Chapter 1",
-		        "url": "http://example.com/1/"
-		    },
-		    {
-		        "image": null,
-		        "start": "01:21:33.010",
-		        "title": "Chapter 2",
-		        "url": "http://example.com/2/"
-		    },
-		    {
-		        "image": null,
-		        "start": "01:17:00.130",
-		        "title": "Chapter 3",
-		        "url": "http://example.com/3/"
-		    }
-		],
-		"content": "<p>Summary</p>",
-		"date": "2013-06-09T22:55:48.500Z",
-		"duration": "01:35:29.914",
-		"episode": "001",
-		"filename": "001",
-		"humandate": "09.06.2013",
-		"subtitle": "<p>It is just the beginning.</p>",
-		"title": "001 Title"
-	}"""
+exportdata["humandate"] = humandate
+
+with open('export.json', 'w') as outfile:
+    json.dump(exportdata, outfile)
+    
+#testprint all data
+pprint(exportdata)
+
+
